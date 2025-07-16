42198a0773224c7d98944e87d9af638c347b1965
Max <resetsistemas2024@gmail.com>
2025-07-15 22:56:33 -0300

    adicionando tags ao head

--- backend/head.php
+++ backend/head.php
@@ -8,8 +8,16 @@ include_once __DIR__ . "/link-padrao.php";
 <head>
   <meta charset="utf-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <meta property="og:title" content="Reset Sistemas" />
+  <meta property="og:description" content="Sistemas ERP, automação e desenvolvimento web sob medida para sua empresa." />
+  <meta property="og:image" content="<?php echo $link_padrao; ?>img/img-sistema/logo-site-reset-sistemas.png" />
+  <meta property="og:url" content="https://resetsistemas.com.br/" />
+  <meta property="og:type" content="website" />
+  <meta property="og:site_name" content="Reset Sistemas" />
+
   <title>Reset Sistemas</title>
-  <link rel="icon" type="image/x-icon" href="<?php echo $link_padrao ;?>img/img-sistema/logo-reset-gradiente.svg">
+
+  <link rel="icon" type="image/x-icon" href="<?php echo $link_padrao; ?>img/img-sistema/logo-reset-gradiente.svg">
   <link rel="preconnect" href="https://fonts.googleapis.com">
   <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
   <link href="https://fonts.googleapis.com/css2?family=Quicksand:wght@300..700&display=swap" rel="stylesheet">
@@ -18,3 +26,16 @@ include_once __DIR__ . "/link-padrao.php";
   <script src="https://cdn.jsdelivr.net/npm/sweetalert2@11"></script>
   <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/sweetalert2@11/dist/sweetalert2.min.css">
   <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
+
+  <!-- Google tag (gtag.js) -->
+  <script async src="https://www.googletagmanager.com/gtag/js?id=G-6JLZJ6EX6T"></script>
+  <script>
+    window.dataLayer = window.dataLayer || [];
+
+    function gtag() {
+      dataLayer.push(arguments);
+    }
+    gtag('js', new Date());
+
+    gtag('config', 'G-6JLZJ6EX6T');
+  </script>
